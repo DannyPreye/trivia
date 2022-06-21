39d4effeacd4ac1cab909d12db5e0522bcbe452d
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-06-21 20:28:28 +0100

    test completed

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 import json
-from urllib import response
+# from urllib import response
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
@@ -26,6 +26,9 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
+
+        self.new_ques = {"question": "what is my name",
+                         "answer": "Daniel", "category": 4, "difficulty": 2}
 
     def tearDown(self):
         """Executed after reach test"""
@@ -52,13 +55,27 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['total_questions'])
 
-    def test_search_question(self):
-        response = self.client().post(
-            '/questions/', json={"searchTerm": "what"})
+    def test_insert(self):
+        res = self.client().post("/questions", json=self.new_ques)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'], True)
+
+    def test_wrong_question_page(self):
+        response = self.client().get("/question?page=400")
         data = json.loads(response.data)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['success'], True)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_delete_question(self):
+        response = self.client().delete("/questions/200")
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertTrue(data['success'], False)
 
 
 # Make the tests conveniently executable
